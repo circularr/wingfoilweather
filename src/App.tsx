5eb6ff99958fab418541544320d09dbdfa5cedbb
circularr <articulation10@gmail.com>
2024-12-05 09:41:31 +0000

    vercel2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { WeatherPredictor } from './components/WeatherPredictor';
+import { Analytics } from '@vercel/analytics/react';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <main>
         <WeatherPredictor />
       </main>
+      <Analytics />
     </div>
   );
 }
